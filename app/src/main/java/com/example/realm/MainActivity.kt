5cff3bc9a52279dd942dea6534a58d5b11125595
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2022-11-19 10:13:45 +0900

    Realm 하는중...

--- app/src/main/java/com/example/realm/MainActivity.kt
+++ app/src/main/java/com/example/realm/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.realm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
 
 class MainActivity : AppCompatActivity() {
+    val config = RealmConfiguration.Builder(schema = setOf(Item::class))
+        .build()
+    val realm: Realm = Realm.open(config)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }

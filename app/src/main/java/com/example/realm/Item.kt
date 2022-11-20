0fd0c16901d6a388b41448bece9c34c650e888fe
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2022-11-20 21:29:05 +0900

    Realm 하는중...

--- app/src/main/java/com/example/realm/Item.kt
+++ app/src/main/java/com/example/realm/Item.kt
@@ -1,16 +1,52 @@
 package com.example.realm
 
-import io.realm.kotlin.types.ObjectId
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.query.RealmResults
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 
-class Item() : RealmObject {
+
+class Dog() : RealmObject {
+    var name: String = ""
+    var age : Int = 0
+}
+
+class Person : RealmObject{
     @PrimaryKey
-    var _id: ObjectId = ObjectId.create()
-    var isComplete: Boolean = false
-    var summary: String = ""
-    var owner_id: String = ""
-    constructor(ownerId: String = "") : this() {
-        owner_id = ownerId
+    var name = ""
+    var dog: Dog? = null
+}
+
+object Database{
+    val configuration = RealmConfiguration.with(schema = setOf(Person::class, Dog::class))
+    val realm = Realm.open(configuration)
+
+    fun write(){
+        val person = Person().apply{
+            name = "Carlo"
+            dog = Dog().apply {
+                name = " Fido" ; age =16
+            }
+        }
+        val manageedPerson =  realm.writeBlocking {
+            copyToRealm(person)
+        }
+    }
+
+    suspend fun writeAsync(){
+        val person = Person().apply{
+            name = "Carlo"
+            dog = Dog().apply {
+                name = " Fido" ; age =16
+            }
+        }
+        realm.write {
+            copyToRealm(person)
+        }
+    }
+    fun query(){
+        //All Person
+        val all: RealmResults<Person> = realm.query<Person>().find()
     }
 }

7cfadb90bbc4045cc2e6ef1cd029a97a46f54ac4
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2022-11-21 22:12:53 +0900

    Realm 하는중...

--- app/src/main/java/com/example/realm/Item.kt
+++ app/src/main/java/com/example/realm/Item.kt
@@ -3,9 +3,14 @@ package com.example.realm
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
 import io.realm.kotlin.ext.query
+import io.realm.kotlin.notifications.InitialRealm
+import io.realm.kotlin.notifications.InitialResults
+import io.realm.kotlin.notifications.ResultsChange
+import io.realm.kotlin.notifications.UpdatedResults
 import io.realm.kotlin.query.RealmResults
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
+import kotlinx.coroutines.flow.collect
 
 
 class Dog() : RealmObject {
@@ -27,7 +32,7 @@ object Database{
         val person = Person().apply{
             name = "Carlo"
             dog = Dog().apply {
-                name = " Fido" ; age =16
+                name = "Fido" ; age =16
             }
         }
         val manageedPerson =  realm.writeBlocking {
@@ -55,6 +60,17 @@ object Database{
     }
     suspend fun queryAsync(){
         //Per having a dog aged more then 7 whit a name starting with "Fi"
-        realm.query<Person>("dog.age")
+        realm.query<Person>("dog.age > $0 AND dog.name BEGINSWITH $1", 7,"Fi")
+            .asFlow()
+            .collect{ results: ResultsChange<Person> ->{
+                when(results){
+                    is InitialResults<Person> -> println("Initial results size ${results.list.size}")
+                    is UpdatedResults<Person> -> println(
+                        "Updated results changes ${results.changes}" +
+                                " deletes ${results.deletions} insertions ${results.insertions}"
+                    )
+                }
+            }
+            }
     }
 }
